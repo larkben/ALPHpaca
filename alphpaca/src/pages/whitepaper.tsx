f51ef130757861321fa02e545d5d9caaf6308ee4
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2023-08-09 21:50:23 -0500

    Start of Whitepaper

--- alphpaca/src/pages/whitepaper.tsx
+++ alphpaca/src/pages/whitepaper.tsx
@@ -8,8 +8,16 @@ import { tokenFaucetConfig } from '@/services/utils'
 export default function WhitePaper() {
   
     return (
-        <div>
-
+        <div className={`${styles.alignCenter} ${styles.textBox}`}>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Tektur&display=swap');
+            </style>
+            <h1 className={`${styles.whitepaperText} ${styles.whitepaperHeading}`}> Whitepaper: </h1>
+            <div>
+                <p className={styles.whitepaperText}> ALPHpaca's is so much more than just NFTs and tokens. 
+                <br/> <br/> Here at ALPHpaca headquarters we believe in an experimental method of development. <br/> <br/> 
+                We aim to be rapidly evolving our potential tools and if we decide to Dapps. </p>
+            </div>
         </div>
     )
 }

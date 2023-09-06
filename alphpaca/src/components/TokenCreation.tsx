8713f011509a29623aa2382beee6ff807490b032
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2023-09-06 09:35:19 -0500

    Token Creation Tool Update

--- alphpaca/src/components/TokenCreation.tsx
+++ alphpaca/src/components/TokenCreation.tsx
@@ -47,28 +47,14 @@ export const TokenAutomationCreate: FC<{
         @import url(&apos;https://fonts.googleapis.com/css2?family=Tektur&display=swap&apos;);
       </style>
       <div style={{color: 'white'}} >
-        <form onSubmit={handleBuildTokenSubmit}>
+        <form onSubmit={handleBuildTokenSubmit} style={{alignContent: 'center', textAlign: 'center'}}>
           <>
-            <h2 className={styles.title} style={{color: 'orange', textAlign: 'center'}}> Alephium Token Builder ({config.network})</h2>
+            <h2 className={styles.title} style={{color: 'white', textAlign: 'center'}}> Alephium Token Builder ({config.network})</h2>
             {/*<p>PublicKey: {context.account?.publicKey ?? '???'}</p>*/}
-            <p style={{color: 'orange', textAlign: 'center'}}> Create your token here with a fee of 1 ALPH for the contract deposit + gas fees. </p>
-            <table>
-              <thead>
-                <tr>
-                  <td>id</td>
-                  <th>group</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr key={addressGroup} style={{ background: 'red', color: 'white' }}>
-                  <td>{config.tokenID}</td>
-                  <td>{addressGroup}</td>
-                </tr>
-              </tbody>
-            </table>
+            <p style={{color: 'white', textAlign: 'center'}}> Create your token here with a fee of 1 ALPH for the contract deposit + gas fees. </p>
             <label htmlFor="symbol">Symbol :</label>
             <input
-                className={styles.input}
+                className={styles.inputToken}
                 type="text"
                 id="symbol"
                 name="symbol"
@@ -78,7 +64,7 @@ export const TokenAutomationCreate: FC<{
             <br/>
             <label htmlFor="name">Name :</label>
             <input
-                className={styles.input}
+                className={styles.inputToken}
                 type="text"
                 id="name"
                 name="name"
@@ -88,7 +74,7 @@ export const TokenAutomationCreate: FC<{
             <br/>
             <label htmlFor="symbol">Decimals :</label>
             <input
-                className={styles.input}
+                className={styles.inputToken}
                 type="number"
                 id="decimals"
                 name="decimals"
@@ -98,7 +84,7 @@ export const TokenAutomationCreate: FC<{
             <br/>
             <label htmlFor="symbol">Supply :</label>
             <input
-                className={styles.input}
+                className={styles.inputToken}
                 type="number"
                 id="supply"
                 name="supply"
@@ -119,29 +105,6 @@ export const TokenAutomationCreate: FC<{
       </div>
 
       <br/>
-
-      <div style={{color: 'orange', textAlign: 'center'}}>
-        <h1 style={{fontSize: 20, color: 'white', textAlign: 'center'}}> About the <Link href='https://github.com/larkben/ALPHpaca/blob/main/alphpaca/contracts/token.ral'> contract </Link> you are creating for your token? Pro Tip: click &apos;contract&apos; </h1>
-        <p> The contract is created with these values; symbol, name, decimals and supply. Symbol is the TICKER for your token and NAME is the name of your token.
-            DECIMALS specify the divisibility of each coin, ie the main attraction of digital currencies over say a gold bar at a cash register; with 2 decimals a 
-            supply of hundred means you have 1 TOKEN that is divisible by 100. Supply is the number of coins you wish to have. As always the contract specifies that
-            you as the minter are the immutable or permanent owner of the coin you make. A destory function as well as event emit handling for future enhancements and features
-            for you the owner.
-        </p>
-      </div>
-      <div style={{color: 'orange', textAlign: 'center'}}>
-        <h1 style={{fontSize: 20, color: 'white', textAlign: 'center'}}> Working with decimals? </h1>
-        <p> Decimals can be complex. <br/>
-            When designing a coin with a supply of 1000 and each coin being divisible by 4 decimals this is how you would do it.
-            <br/>
-            First type the number of decimals in as zeros in the supply. IE 0000 4 decimals
-            <br/> 
-            Second type the supply you want. IE 1000
-            <br/>
-            Final result should look like this. IE decimals: 4 supply: 10000000 ~ 1000 coins with 4 decimals
-        </p>
-      </div>
-
     </>
   )
 }
--- alphpaca/src/pages/token_create.tsx
+++ alphpaca/src/pages/token_create.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import { AlephiumConnectButton } from '@alephium/web3-react'
+import { AlephiumConnectButton, AlephiumWalletProvider } from '@alephium/web3-react'
 import { TokenFaucetConfig, TokenCreate, TokenTemplate } from '@/services/utils'
 import { TokenDapp } from '@/components/PacaFaucet'
 import { DevBoard } from '@/components/DevDashboard'
@@ -12,20 +12,11 @@ import { CreateToken, CreateTokenInstance, CreateTokenTypes } from 'artifacts/ts
 import Router from './router'
 import Link from 'next/link'
 import tokenCreate from 'scripts/2_token_create'
+import WhitePaper from './whitepaper'
 
 // Testnet: https://wallet-v20.testnet.alephium.org
 
 // Mainnet: https://node-alephium.ono.re/
-
-const nodeProvider = new NodeProvider('https://node-alephium.ono.re/')
-
-async function getChainInfo() {
-    const result = await nodeProvider.blockflow.getBlockflowChainInfo({
-        fromGroup: 0,
-        toGroup: 0
-      })
-      return result
-}
 
 //const contractAddress = '29NQz5dKwACqHw5beJ25v6eZQpxAC7rPYqK4xpFYc7CqN' // ContractAddress
 
@@ -40,20 +31,6 @@ async function getTokenEvents() {
 
 
 export default function AutoTokenMake() {
-
-    const [blockHeight, setBlockHeight] = useState<number>()
-
-    getChainInfo()
-    .then(data => {
-        if (data) {
-            const blockHeight = data.currentHeight
-            console.log('Current Height:', blockHeight)
-            setBlockHeight(blockHeight)
-        }
-        else {
-            console.log('No Data Available')
-        }
-    })
 
     /*
     getTokenEvents()
@@ -77,11 +54,21 @@ export default function AutoTokenMake() {
                 <button className={styles.button}> <Link href='/tools' className={styles.noDeco} style={{color: 'white'}}>  Back to Tools </Link> </button>
             </div>
             <br/>
-            <p style={{color: 'orange'}}> The current block height on group 0 is: {blockHeight} </p>
-            <AlephiumConnectButton></AlephiumConnectButton>
-            {/*<TokenDapp config={TokenFaucetConfig}></TokenDapp> This is the $PACA faucet; actively not in use at the moment*/}
-            {/*<DevBoard config={TokenFaucetConfig}></DevBoard> This is the dev dashboard*/}
-            <TokenAutomationCreate config={TokenFaucetConfig}></TokenAutomationCreate>
+            <div style={{backgroundColor: 'blue', padding: 20}} className={styles.showBorder}>
+                <AlephiumConnectButton></AlephiumConnectButton>
+                <h2 style={{color: 'white', textAlign: 'center'}}> Please connect wallet before creating token! </h2>
+                {/*<TokenDapp config={TokenFaucetConfig}></TokenDapp> This is the $PACA faucet; actively not in use at the moment*/}
+                {/*<DevBoard config={TokenFaucetConfig}></DevBoard> This is the dev dashboard*/}
+                <TokenAutomationCreate config={TokenFaucetConfig}></TokenAutomationCreate>
+            </div>
+            <div>
+                <h1 style={{color: 'white', textAlign: 'center'}}> Using the Token Creation Tool? </h1>
+                <p style={{color: 'white', textAlign: 'center'}}> Decimals are the most complex component of this tool. <br/> When using decimals the number you enter is how many zeros you should to add to your supply. <br/> <br/>
+                For Example: If you enter 4 Decimals and Supply of 1000 you will get a token with a supply of 1. If you want a supply of 1000 you should add 4 zeros. 10000000 is what the supply should be. <br/> <br/> 
+                The Math: 6 Decimals 10000 Supply. Supply should be 10000(000000); do not enter parenthesis, they are meant to show the supply decimal logic. </p>
+                <h1 style={{color: 'white', textAlign: 'center'}}> Redeeming ALPH Deposit? </h1>
+                <p style={{color: 'white', textAlign: 'center'}}> We are currently scaling our DB and platform to accomadate this. </p>
+            </div>
         </div>
     )
 }
